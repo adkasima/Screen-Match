84541ec425f05ddcb2db8e78290c17a549da0ed8
Adriel <adrielkasima@gmail.com>
2024-06-16 12:25:10 -0300

    :tada: screen match

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFIlme = new Filme();
+
+        meuFIlme.setNome("O Poderoso Chefão");
+        meuFIlme.setAnoDeLancamento(1970);
+        meuFIlme.setDuracaoEmMinutos(180);
+
+        meuFIlme.exibeFichaTecnica();
+        meuFIlme.avalia(10);
+        meuFIlme.avalia(10);
+        meuFIlme.avalia(20);
+        System.out.println(meuFIlme.getTotalDeAvaliacoes());
+        System.out.println(meuFIlme.retornaMediaAvaliacoes());
+
+
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -0,0 +1,63 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Filme {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDeAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+    }
+
+    public void avalia(double nota) {
+        if(nota > 10) {
+            nota = 10;
+        }
+        if(nota < 0) {
+            nota = 0;
+        }
+        somaDeAvaliacoes += nota;
+        totalDeAvaliacoes++;
+
+    }
+
+    public double retornaMediaAvaliacoes() {
+        return somaDeAvaliacoes / totalDeAvaliacoes;
+    }
+}
